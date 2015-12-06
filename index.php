f64092582933e26285d408cfc73128d0fb971cb3
Niels van Velzen <HetIsNiels@users.noreply.github.com>
2015-12-06 11:35:35 +0100

    Show version at index

---
+++ index.php
@@ -0,0 +1,15 @@
+Coming soon &copy;<br />
+<?php
+if(file_exists(__DIR__ . '/version')){
+	$version = file_get_contents(__DIR__ . '/VERSION');
+	
+	echo '<em>Build from commit <a href="https://github.com/HetIsNiels/Portfolio/commit/' . $version . '">' . $version . '</a></em>';
+}
+?>
+<script>
+var redirect = function () {
+	document.location = 'https://github.com/HetIsNiels';
+};
+
+setTimeout(redirect, 1000);
+</script>
