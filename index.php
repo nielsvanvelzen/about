e62bdfcddccb45f7cc2802c4fb6b39fb82c84716
Niels van Velzen <git@hetisniels.nl>
2016-10-19 11:35:57 +0200

    Test for buildscripts on master branch

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
-Coming soon &copy;<br />
-<?php
-if(file_exists(__DIR__ . '/version')){
-	$version = file_get_contents(__DIR__ . '/version');
-	
-	echo '<em>Build from commit <a href="https://github.com/HetIsNiels/Portfolio/commit/' . $version . '">' . $version . '</a></em>';
-}
-?>
-<script>
-var redirect = function () {
-	document.location = 'https://github.com/HetIsNiels';
-};
-
-setTimeout(redirect, 1000);
-</script>
+<h1>Niels van Velzen</h1>
+<em>Website is still in development.</em>
