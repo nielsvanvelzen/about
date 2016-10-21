360a26f8a00831a1039837d6505c5e1683b687f6
Niels van Velzen <git@hetisniels.nl>
2016-10-21 23:44:18 +0200

    Add "About me" & "Skills". Also removed the border-radius :)

--- index.php
+++ index.php
@@ -1,28 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
+	<meta charset="utf-8"/>
 
-	<meta name="author" content="Niels van Velzen" />
-	<meta name="description" content="Portfolio of Niels van Velzen" />
-	<meta name="keywords" content="Niels van Velzen, Niels, van Velzen, portfolio, website, software, full stack developer, developer" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<meta name="author" content="Niels van Velzen"/>
+	<meta name="description" content="Portfolio of Niels van Velzen"/>
+	<meta name="keywords" content="Niels van Velzen, Niels, van Velzen, portfolio, website, software, full stack developer, developer"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+
+	<meta name="theme-color" content="#00695C"/>
 
 	<title>Niels van Velzen</title>
 
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400"/>
 	<link rel="stylesheet" href="assets/lib/fontawesome/css/font-awesome.min.css"/>
 	<link rel="stylesheet" href="assets/css/style.css"/>
 </head>
 <body>
 	<div class="profile">
 		<header>
-			<img src="assets/img/profile.jpg" class="avatar" alt="Niels van Velzen" />
+			<img src="assets/img/profile.jpg" class="avatar" alt="Niels van Velzen"/>
 			<div class="bio">
 				<h2>Niels van Velzen</h2>
 				<h3>Full Stack Developer</h3>
+				<!-- Maybe put quick action(s) here? [mail] -->
 			</div>
 		</header>
+		<main>
+			<section>
+				<h1>About me</h1>
+				<p>I am a Full Stack Developer from the Netherlands. In my daily life I create websites, software, apps
+					and games, this is my job and my passion.</p>
+			</section>
+			<section>
+				<h1>Skills</h1>
+				<p>A set of all kinds of things (mostly programming languages) in which I have good skills.</p>
+				<ul class="labels">
+					<li>Javascript</li>
+					<li title="Relatively new language for the Java Virtual Machine">Kotlin</li>
+					<li>PHP</li>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>Java</li>
+					<li>C#</li>
+
+					<li title="Amazon Web Services">AWS</li>
+					<li title="Server-side Javascript">NodeJS</li>
+				</ul>
+			</section>
+		</main>
 		<div class="shortcuts">
 			<a href="https://twitter.com/HetIsNiels" target="_blank" class="shortcut twitter" title="@HetIsNiels on Twitter">
 				<i class="fa fa-twitter"></i>
@@ -40,10 +66,17 @@
 	</div>
 
 	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function () {
+					(i[r].q = i[r].q || []).push(arguments)
+				}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 		ga('create', 'UA-30987193-7', 'auto');
 		ga('send', 'pageview');
